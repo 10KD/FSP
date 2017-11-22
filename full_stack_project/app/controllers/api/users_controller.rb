4e0a9b3db01506b2c8a5dc35b148ff33e5e6b8e2
KDG <mkim.don@gmail.com>
2017-11-22 09:36:00 -0800

    working on Auth

--- full_stack_project/app/controllers/api/users_controller.rb
+++ full_stack_project/app/controllers/api/users_controller.rb
@@ -1,4 +1,7 @@
 class Api::UsersController < ApplicationController
+
+
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -8,6 +11,9 @@ class Api::UsersController < ApplicationController
       render json: @user.errors.full_messages, status: 422
     end
   end
+
+
+
   private
   def user_params
     params.require(:user).permit(:username, :password)
